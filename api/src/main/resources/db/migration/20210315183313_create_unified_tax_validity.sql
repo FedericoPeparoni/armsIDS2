00c9a7085edab094317a99610d1c698b252f2738
mcappella <marco.cappella@nais-solutions.it>
2021-03-19 19:28:05 +0100

    Removed not-null constraint from unifiedtaxvalidity table for validity dates from and to

--- api/src/main/resources/db/migration/20210315183313_create_unified_tax_validity.sql
+++ api/src/main/resources/db/migration/20210315183313_create_unified_tax_validity.sql
@@ -1,8 +1,8 @@
 create table unified_tax_validity
 (
     id   serial primary key,
-    from_validity_year timestamp with time zone not null,
-    to_validity_year timestamp with time zone not null,
+    from_validity_year timestamp with time zone,
+    to_validity_year timestamp with time zone,
     created_at timestamp with time zone  not null,
     created_by varchar (50)  not null,
     updated_at timestamp with time zone ,
