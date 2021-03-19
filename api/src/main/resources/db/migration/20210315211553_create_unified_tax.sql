0e5e365281269cf1f16229968556700a82de2cb4
mcappella <marco.cappella@nais-solutions.it>
2021-03-19 19:30:51 +0100

    Bugfixing on changelog for the generation of the unified_tax table

--- api/src/main/resources/db/migration/20210315211553_create_unified_tax.sql
+++ api/src/main/resources/db/migration/20210315211553_create_unified_tax.sql
@@ -4,7 +4,7 @@ create table unified_tax
     from_manufacture_year timestamp with time zone,
     to_manufacture_year timestamp with time zone,
     rate varchar (255) not null,
-    validity_id int references account_types (id),
+    validity_id int references unified_tax_validity (id),
     created_at timestamp with time zone  not null,
     created_by varchar (50)  not null,
     updated_at timestamp with time zone ,
