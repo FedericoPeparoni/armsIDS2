714110fc90c52087d88fa07ff3a126ce72ad8a4f
agallinella <agallinella@DESKTOP-583FEEH>
2021-03-26 16:34:37 +0100

    Aggiornamenti,modifiche traduzioni en/es. Modifica tabella UnifiedTaxManagment

--- api/src/main/resources/db/translations/all_translations.sql
+++ api/src/main/resources/db/translations/all_translations.sql
@@ -14541,24 +14541,11 @@ select add_languages_by_json('[
   {
     "code": "es",
     "token": "Billing Interval *",
-    "val": "Intervalo de Facturación",
+    "val": "Intervalo de Facturación *",
     "part": "frontend",
     "created_by": "system"
   },
-  {
-    "code": "en",
-    "token": "Billing Period",
-    "val": "Billing Period",
-    "part": "frontend",
-    "created_by": "system"
-  },
-  {
-    "code": "es",
-    "token": "Billing Period",
-    "val": "Periodo de Facturación",
-    "part": "frontend",
-    "created_by": "system"
-  },
+  
   {
     "code": "en",
     "token": "Billing Period *",
