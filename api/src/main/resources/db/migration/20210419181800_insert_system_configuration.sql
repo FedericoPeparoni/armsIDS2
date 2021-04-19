0022e4fa20eb1616b545229dbe37076130b60af3
fpeparoni <fpeparoni@LAPTOP-9I813KEF>
2021-04-19 15:25:34 +0200

    modifica System Configuration

---
+++ api/src/main/resources/db/migration/20210419181800_insert_system_configuration.sql
@@ -0,0 +1,26 @@
+do $$
+declare
+    system_item_types_id integer;
+begin
+    system_item_types_id := nextval('abms.system_item_types_id_seq');
+
+
+INSERT INTO abms.system_item_types(
+    id, name)
+VALUES (system_item_types_id, 'Unified Tax');
+
+
+INSERT INTO abms.system_configurations(
+    id, item_name, item_class, data_type, units, range, created_by)
+    
+    
+VALUES (DEFAULT, 'Agricultural Discount (%)', system_item_types_id, 5, 'percent', '0,100', 'system' );
+       
+
+
+INSERT INTO abms.system_configurations(
+    id, item_name, item_class, data_type, units, range, created_by)
+ 
+VALUES (DEFAULT, 'Flight School Discount (%)', system_item_types_id, 5, 'percent', '0,100', 'system');
+  
+ end $$ LANGUAGE plpgsql;
