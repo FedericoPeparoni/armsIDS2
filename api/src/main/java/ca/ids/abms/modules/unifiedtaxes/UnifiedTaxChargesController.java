b3a8e6876421dd5cf8168478045150e3ab368a3b
agallinella <agallinella@DESKTOP-583FEEH>
2021-05-04 15:33:16 +0200

    UnifiedTaxCharges Controller working progress

--- api/src/main/java/ca/ids/abms/modules/unifiedtaxes/UnifiedTaxChargesController.java
+++ api/src/main/java/ca/ids/abms/modules/unifiedtaxes/UnifiedTaxChargesController.java
@@ -1,5 +1,57 @@
 package ca.ids.abms.modules.unifiedtaxes;
 
-public class UnifiedTaxChargesController {
+import java.net.URISyntaxException;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import ca.ids.abms.modules.common.controllers.AbmsCrudController;
+
+@RestController
+@RequestMapping(UnifiedTaxChargesController.ENDPOINT)
+public class UnifiedTaxChargesController{
+
+	
+	static final String ENDPOINT = "/api/unified-tax-charges";
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UnifiedTaxChargesController.class);
+	
+	
+	private final UnifiedTaxChargesService unifiedTaxChargesService ;
+	@Autowired
+	public UnifiedTaxChargesController(final UnifiedTaxChargesService unifiedTaxChargesService) {
+		this.unifiedTaxChargesService=unifiedTaxChargesService;
+	}
+	
+	
+	@GetMapping(path = "/list")
+    @PreAuthorize("hasAuthority('unified_tax_charge_view')")
+	public ResponseEntity<List<UnifiedTaxCharges>> getAllUnifiedTaxCharges(){
+		return ResponseEntity.ok(unifiedTaxChargesService.findAll());
+				
+
+	}
+	
+	
+	
+   /* @PostMapping()
+    @PreAuthorize("hasAuthority('unified_tax_modify')")
+    public ResponseEntity<UnifiedTaxCharges> save(final UnifiedTaxCharges charges){
+             return ResponseEntity.ok().body(unifiedTaxChargesService.save(charges));
+            } */
 }
+
--- api/src/main/java/ca/ids/abms/modules/unifiedtaxes/UnifiedTaxChargesService.java
+++ api/src/main/java/ca/ids/abms/modules/unifiedtaxes/UnifiedTaxChargesService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +18,7 @@ public class UnifiedTaxChargesService  {
 
 	
     private final UnifiedTaxChargesRepository unifiedTaxChargesRepository;
-    
+    @Autowired
     UnifiedTaxChargesService(final UnifiedTaxChargesRepository unifiedTaxChargesRepository) {
     	this.unifiedTaxChargesRepository = unifiedTaxChargesRepository;
     }
@@ -35,4 +36,12 @@ public class UnifiedTaxChargesService  {
 		return unifiedTaxChargesRepository.findAll() ;
 		
 	}	
+    
+    
+    
+    
+    
+    
+    
+    
 }
