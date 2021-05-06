8c7ba76b4bd514084a5b9fb08f4edd353ace475e
amonteleone <antonio.monteleone@nais-solutions.it>
2021-05-06 15:18:42 +0200

    Corretto bug su pagamenti fatti in ARS su fatture multiple emesse in USD

--- api/src/main/java/ca/ids/abms/modules/reports2/transaction/TransactionReceiptCreator.java
+++ api/src/main/java/ca/ids/abms/modules/reports2/transaction/TransactionReceiptCreator.java
@@ -371,12 +371,19 @@ public class TransactionReceiptCreator {
             xx.invoiceDateStr = reportHelper.formatDateUtc (transactionPayment.getBillingLedger().getInvoiceDateOfIssue(), dateFormatter);
 
             Currency paymentCurrency = transactionPayment.getCurrency();
-            xx.localAmount = roundingUtils.calculateSingleRoundedValue(Math.abs(transactionPayment.getAmount()), paymentCurrency, aviation);
+            
+            xx.localAmount = roundingUtils.calculateSingleRoundedValue(Math.abs(transactionPayment.getAmount()), paymentCurrency, aviation);            
             xx.localAmountStr = reportHelper.formatCurrency (xx.localAmount, paymentCurrency);
             xx.localAmountStrWithCurrencySymbol = reportHelper.formatCurrencyWithSymbol (xx.localAmount, paymentCurrency);
 
+/*            
             Double paymentAmount = cachedCurrencyConverter.convertCurrency(transactionPayment.getAmount(), paymentCurrency, transactionPaymentCurrency);
             xx.paymentAmount = roundingUtils.calculateSingleRoundedValue(Math.abs(paymentAmount), transactionPaymentCurrency, aviation);
+*/          
+            double percentage = transactionPayment.getAmount() / transaction.getAmount();
+            xx.paymentAmount = data.global.paymentAmount * percentage;
+            xx.paymentAmount = roundingUtils.calculateSingleRoundedValue(Math.abs(xx.paymentAmount), paymentCurrency, aviation);
+            
             xx.paymentAmountStr = reportHelper.formatCurrency (xx.paymentAmount, transactionPaymentCurrency);
             xx.paymentAmountStrWithCurrencySymbol = reportHelper.formatCurrencyWithSymbol (xx.paymentAmount, transactionPaymentCurrency);
 
