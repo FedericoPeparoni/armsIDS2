70eee14d6330b3b385a872fadbf838fe02e71be5
fpeparoni <fpeparoni@LAPTOP-9I813KEF.mshome.net>
2021-07-09 12:14:50 +0200

    traduzione

--- api/src/main/resources/db/translations/releasenotes/v1.5/20210709093701_changelog_lang_1.5.3.sql
+++ api/src/main/resources/db/translations/releasenotes/v1.5/20210709093701_changelog_lang_1.5.3.sql
@@ -3,15 +3,14 @@ select add_languages_by_json('[
     "code": "en",
     "token": "No Unified Tax aircraft linked to this Account",
     "val": "No Unified Tax aircraft linked to this Account",
-    "part": "backend",
+    "part": "frontend",
     "created_by": "system"
   },
-
 {
     "code": "es",
     "token": "No Unified Tax aircraft linked to this Account",
     "val": "Esta Cuenta no tiene Aeronaves de TU asociada",
-    "part": "backend",
+    "part": "frontend",
     "created_by": "system"
   }
 
