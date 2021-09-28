7c691a09c2d7e845cd179cf621d6908a1e412a37
fpeparoni <fpeparoni@LAPTOP-9I813KEF.mshome.net>
2021-09-28 16:59:06 +0200

    .

---
+++ api/src/main/resources/db/translations/releasenotes/v1.5/20210461513187_changelog_lang_1.5.3.sql
@@ -0,0 +1,183 @@
+select add_languages_by_json('[
+  {
+    "code": "en",
+    "token": "UNIFIED_TAX",
+    "val": "unifiedtax",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "UNIFIED_TAX",
+    "val": "tasaunificada",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "en",
+    "token": "Add a Unified Tax item",
+    "val": "Add a Unified Tax item",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Add a Unified Tax item",
+    "val": "Añadir de Articulo Tasa Unificada",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "en",
+    "token": "Unified Tax Formula",
+    "val": "Unified Tax Formula",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Unified Tax Formula",
+    "val": "Fórmula Fiscal Unificada",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "en",
+    "token": "Annual",
+    "val": "Annual",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Annual",
+    "val": "Anual",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "en",
+    "token": "Partial",
+    "val": "Partial",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Partial",
+    "val": "Parcial",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "en",
+    "token": "Applicable Unified Tax *",
+    "val": "Applicable Unified Tax *",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Applicable Unified Tax *",
+    "val": "Impuesto unificado aplicable *",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+
+  {
+    "code": "en",
+    "token": "Looking for billable unified tax aircraft",
+    "val": "Looking for billable unified tax aircraft",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Looking for billable unified tax aircraft",
+    "val": "Buscando aeronaves facturables de Tasa Unificada",
+    "part": "frontend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "en",
+    "token": "Looking for billable unified tax aircraft",
+    "val": "Looking for billable unified tax aircraft",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Looking for billable unified tax aircraft",
+    "val": "Buscando aeronaves facturables de Tasa Unificada",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+
+  {
+    "code": "en",
+    "token": "Validating the flight movement list",
+    "val": "Validating the flight movement list",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "Validating the flight movement list",
+    "val": "Validando la lista de movimientos de vuelos",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+
+  {
+    "code": "en",
+    "token": "flightschool",
+    "val": "Flight school",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "flightschool",
+    "val": "Entrenamiento",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+
+  {
+    "code": "en",
+    "token": "agriculture",
+    "val": "Agriculture",
+    "part": "backend",
+    "created_by": "system"
+  },
+
+  {
+    "code": "es",
+    "token": "agriculture",
+    "val": "Agricultura",
+    "part": "backend",
+    "created_by": "system"
+  }
+
+
+]');
