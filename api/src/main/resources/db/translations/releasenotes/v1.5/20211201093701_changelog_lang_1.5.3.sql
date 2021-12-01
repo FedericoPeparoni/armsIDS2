6f2680caa5c7556a030eacfc71ca212846d14c5a
fpeparoni <fpeparoni@LAPTOP-9I813KEF.mshome.net>
2021-12-01 11:02:37 +0100

    .

---
+++ api/src/main/resources/db/translations/releasenotes/v1.5/20211201093701_changelog_lang_1.5.3.sql
@@ -0,0 +1,18 @@
+select add_languages_by_json('[
+{
+    "code": "en",
+    "token": "No Unified Tax aircraft linked to this Account",
+    "val": "No Unified Tax aircraft linked to this Account",
+    "part": "frontend",
+    "created_by": "system"
+  },
+{
+    "code": "es",
+    "token": "No Unified Tax aircraft linked to this Account",
+    "val": "Esta Cuenta no tiene Aeronaves de TU asociada",
+    "part": "frontend",
+    "created_by": "system"
+  }
+
+
+]');
